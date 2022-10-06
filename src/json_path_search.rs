8da1b14a9b0aae41eae3fc7940e2957e6131681e
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-10-06 16:50:12 +0100

    Improved Json path search

--- src/json_path_search.rs
+++ src/json_path_search.rs
@@ -11,8 +11,16 @@ pub(crate) fn process_file_with_json_path(path: PathBuf, search_expression_optio
     let path_clone = path.clone();
     let json_path_str = search_expression_option.as_ref().unwrap();
     let json_string_res = fs::read_to_string(path_clone);
-    let json_string = json_string_res.unwrap();
-    let finder = JsonPathFinder::from_str(&json_string, json_path_str).unwrap();
-    let slice_of_data: Vec<&Value> = finder.find_slice();
-    output.output(format!("{} :: {:?}", path.to_str().unwrap(), slice_of_data).as_str());
+    match json_string_res {
+        Ok(json_string) => {
+            let finder = JsonPathFinder::from_str(&json_string, json_path_str).unwrap();
+            let slice_of_data: Vec<&Value> = finder.find_slice();
+            if !slice_of_data.is_empty() {
+                output.output(format!("{} :: {:?}", path.to_str().unwrap(), slice_of_data).as_str());
+            }
+        }
+        Err(e) => {
+            output.err_output(format!("Error occurred: {:?}", e).as_str());
+        }
+    }
 }
