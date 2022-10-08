3bbdb5d51a3d04f90e4106498f865b6223e5d095
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-10-08 11:08:20 +0100

    Removed compile warnings

--- src/pdf_search.rs
+++ src/pdf_search.rs
@@ -1,11 +1,9 @@
 extern crate lopdf;
 
 use std::path::PathBuf;
-use lopdf::Document;
-use crate::{find_simple, OutputPrinter};
 use pdf_extract::extract_text;
+use crate::OutputPrinter;
 use crate::finders::find_simple_pos;
-
 
 pub(crate) fn process_pdf_simple_search(path: PathBuf, search_expression_option: &Option<String>,
                                         output: &mut dyn OutputPrinter) {
@@ -15,12 +13,12 @@ pub(crate) fn process_pdf_simple_search(path: PathBuf, search_expression_option:
         Ok(content) => {
             let json_path_str = search_expression_option.as_ref().unwrap();
             let found = find_simple_pos(content.as_str(), json_path_str);
-            if(found.is_some()) {
+            if found.is_some() {
                 output.output_with_stats(format!("{} :: {}", file_name, found.unwrap()).as_str());
             }
         }
         Err(e) => {
-            output.err_output(format!("Could not extract text from '{}'", file_name).as_str());
+            output.err_output(format!("Could not extract text from '{}': {:?}", file_name, e).as_str());
         }
     }
 }
