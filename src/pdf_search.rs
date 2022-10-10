ae6a8371a327302f628365fdaa0d595a2d6aaef8
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-10-10 20:07:14 +0100

    Added smal preview for PDF files

--- src/pdf_search.rs
+++ src/pdf_search.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use pdf_extract::extract_text;
 use crate::OutputPrinter;
 use crate::finders::find_simple_pos;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub(crate) fn process_pdf_simple_search(path: PathBuf, search_expression_option: &Option<String>,
                                         output: &mut dyn OutputPrinter) {
@@ -11,10 +12,16 @@ pub(crate) fn process_pdf_simple_search(path: PathBuf, search_expression_option:
     let file_name = path.to_str().expect("Could not extract file name from path");
     match extracted {
         Ok(content) => {
-            let json_path_str = search_expression_option.as_ref().unwrap();
-            let found = find_simple_pos(content.as_str(), json_path_str);
+            let search_expression = search_expression_option.as_ref().unwrap();
+            let content_str = content.as_str();
+            let found = find_simple_pos(content_str, search_expression);
             if found.is_some() {
-                output.output_with_stats(format!("{} :: {}", file_name, found.unwrap()).as_str());
+                let example_vec = UnicodeSegmentation::grapheme_indices(content_str, true)
+                    .collect::<Vec<(usize, &str)>>();
+                let example = &example_vec[..found.unwrap() + search_expression.len()].iter()
+                    .map(|x| x.1).collect::<String>();
+                output.output_with_stats(format!("{} :: {} :: @@{}@@", file_name, found.unwrap(), example)
+                    .as_str());
             }
         }
         Err(e) => {
